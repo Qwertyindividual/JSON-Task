27d8becdb74537b8018ba79270fb3a6c3e42057d
Qwertyindividual <brandtkauthar@gmail.com>
2022-11-28 14:02:11 +0200

    json

---
+++ json.js
@@ -0,0 +1,10 @@
+fetch('index.json').then(function (response) {
+    return response.json();
+}).then(function (obj) {
+     document.write(obj);
+}).catch(function (error) {
+alert (
+    "Somethig went wrong with retrieving index"
+);
+console.error(error);
+})
